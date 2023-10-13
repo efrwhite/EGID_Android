d948501f675e07ee91bc18fac04e93b9ba202bca
Elizabeth White Baker <efrwhite@aol.com>
2023-10-13 16:27:50 -0400

    Commit for sign-up and sign-in

--- app/src/main/java/com/example/fire/MainActivity.kt
+++ app/src/main/java/com/example/fire/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.fire
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
