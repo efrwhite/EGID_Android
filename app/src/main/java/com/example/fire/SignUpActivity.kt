354ea8dd371e3f9481e15b821b49c65ee5c5d5f5
b-massie <massieb2@vcu.edu>
2024-01-29 11:55:37 -0500

    Add Firebase .json file, remove back buttons from layouts, added SignUpActivity and SignInActivity to AndroidManifest.xml

--- app/src/main/java/com/example/fire/SignUpActivity.kt
+++ app/src/main/java/com/example/fire/SignUpActivity.kt
@@ -17,8 +17,7 @@ class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_signup)
         // Initialize Firebase authentication
         firebaseAuth = FirebaseAuth.getInstance()
 
