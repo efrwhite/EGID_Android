06d0eb006306584c03b51d191a085bca11953a11
b-massie <massieb2@vcu.edu>
2024-02-12 11:35:06 -0500

    Quality of Life and Symptoms Trackers

--- app/src/main/java/com/example/fire/SymptomCheckerActivity.kt
+++ app/src/main/java/com/example/fire/SymptomCheckerActivity.kt
@@ -1,4 +1,10 @@
 package com.example.fire
 
-class SymptomCheckerActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class SymptomCheckerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_symptom_score_checker)
+    }
 }
