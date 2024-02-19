d659325bd7448bee6e157136669af72fd6be4e40
b-massie <massieb2@vcu.edu>
2024-02-19 11:32:33 -0500

    Created new activities and layouts
    Created new activities for listed meds, documents and diet plan and layouts for each.

--- app/src/main/java/com/example/fire/PlanActivity.kt
+++ app/src/main/java/com/example/fire/PlanActivity.kt
@@ -5,8 +5,33 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 class PlanActivity : AppCompatActivity() {
+
+    private lateinit var dietButton: Button
+    private lateinit var medicationsButton: Button
+    private lateinit var documentsButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_yourplan)
+
+        //Initialize buttons
+        dietButton = findViewById(R.id.dietButton)
+        medicationsButton = findViewById(R.id.medicationsButton)
+        documentsButton = findViewById(R.id.documentsButton)
+
+        //set onClickListeners for Buttons
+        dietButton.setOnClickListener {
+            val intent = Intent(this, DietPlanActivity :: class.java)
+            startActivity(intent)
+        }
+
+        medicationsButton.setOnClickListener {
+            val intent = Intent(this, MedicationsActivity :: class.java)
+            startActivity(intent)
+        }
+
+        documentsButton.setOnClickListener {
+            val intent = Intent(this, DocumentsActivity :: class.java)
+            startActivity(intent)
+        }
     }
 }
