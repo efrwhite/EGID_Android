4660697a48e22a51a69a5650bd78f2d048eb1fd5
laurenviado <lauren.viado1@gmail.com>
2024-03-11 11:45:12 -0400

    3/11
    3/11

--- app/src/main/java/com/example/fire/YourPlanActivity.kt
+++ app/src/main/java/com/example/fire/YourPlanActivity.kt
@@ -1,10 +1,8 @@
 package com.example.fire
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-class PlanActivity : AppCompatActivity() {
+class YourPlanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_yourplan)
