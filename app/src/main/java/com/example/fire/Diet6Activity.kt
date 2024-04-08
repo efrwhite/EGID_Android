046f95d9cb51c3bede099edab2a4307913c23b86
b-massie <massieb2@vcu.edu>
2024-04-08 11:28:33 -0400

    update and delete from lauren

--- app/src/main/java/com/example/fire/Diet6Activity.kt
+++ app/src/main/java/com/example/fire/Diet6Activity.kt
@@ -6,8 +6,19 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Diet6Activity : AppCompatActivity() {
+    private lateinit var allergiesButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_diet6)
+        setContentView(R.layout.activity_diet1)
+
+        //initialize buttons
+        allergiesButton = findViewById(R.id.allergiesButton)
+
+        // Set onClickListeners for the buttons
+        allergiesButton.setOnClickListener {
+            val intent = Intent(this, AllergiesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
